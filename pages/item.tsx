2b1d4a2fa57f6627deeb329da48f4e7ba42af05c
TheoMer <6e240757@use.startmail.com>
2020-09-24 16:50:22 +0100

    Added orderlist component

--- pages/item.tsx
+++ pages/item.tsx
@@ -1,22 +1,39 @@
+import React, {FC} from "react";
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
 import SingleItem from '../components/SingleItem';
-import { withRouter } from "next/router";
 
 import SingleItemStyles from '../public/static/SingleItemStyles.css';
 
-const Item = props => (
-  <div>
-    <style dangerouslySetInnerHTML={{ __html: SingleItemStyles }} />
-    <SingleItem id={props.query.id} user_ip={props.userIP} user_Agent={props.userAgent} url={props.router.asPath} urlReferer={props.urlReferer} />
-  </div>
-);
-
-// Obtain User IP on initial site load
-Item.getInitialProps = async ({ req }) => {
-  const userIP = req ? (req.headers['x-real-ip'] || req.connection.remoteAddress) : false;
-  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
-  const urlReferer = req ? req.headers['referer'] : document.referrer;
-  
-  return { userIP, userAgent, urlReferer }
+interface Props {
+  query?: {
+    id: string
+  };
+  userAgent?: string;
+  userIP?: any;
+  urlReferer?: string;
 }
 
-export default withRouter(Item);
+// const Item: FC<Props> = ({ query, userAgent, userIP, urlReferer }) => {
+
+const Item: NextPage<Props> = ({ query, userAgent, userIP, urlReferer }) => {
+
+  const router = useRouter();
+
+  return(
+    <div>
+      <style dangerouslySetInnerHTML={{ __html: SingleItemStyles }} />
+      <SingleItem id={query.id} user_ip={userIP} user_Agent={userAgent} url={router.asPath} urlReferer={urlReferer} />
+    </div>
+  );
+}
+
+Item.getInitialProps = async ({ req }) => {
+  const userAgent = req ? req.headers['user-agent'] : navigator.userAgent;
+  const userIP = req ? (req.headers['x-real-ip'] || req.connection.remoteAddress) : false;
+  const urlReferer = req ? req.headers['referer'] : document.referrer;
+
+  return { userAgent, userIP, urlReferer }
+}
+
+export default Item
