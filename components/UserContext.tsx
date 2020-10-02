174d94c465fb61e4ff51bd47b7aff62606dc3f92
TheoMer <6e240757@use.startmail.com>
2020-10-02 02:14:20 +0100

    Reset project

--- components/UserContext.tsx
+++
@@ -1,8 +0,0 @@
-// Taken from: https://reacttricks.com/sharing-global-data-in-next-with-custom-app-and-usecontext-hook/
-// https://daveceddia.com/usecontext-hook/
-import React, { memo } from 'react';
-import { createContext } from 'react';
-
-const UserContext = createContext();
-
-export default memo(UserContext);
